f08278bf8b1f2a034c2a9964c86f3313130d709c
bao <zhou_zcb@163.com>
2013-10-14 23:27:46 +0800

    none

---
+++ src/main/java/com/fusionspy/beacon/site/wls/dao/WlsServerDao.java
@@ -0,0 +1,12 @@
+package com.fusionspy.beacon.site.wls.dao;
+// Generated 2013-10-14 0:49:30 by One Data Tools 1.0.0
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+import com.fusionspy.beacon.site.wls.entity.WlsServer;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface WlsServerDao extends PagingAndSortingRepository<WlsServer, Integer> {
+}
+
