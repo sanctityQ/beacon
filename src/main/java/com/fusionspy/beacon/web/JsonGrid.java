e3b9cdacce5c1377254a818f54209e280d480891
bao <zhou_zcb@163.com>
2013-10-17 23:52:39 +0800

    JsonGrid

---
+++ src/main/java/com/fusionspy/beacon/web/JsonGrid.java
@@ -0,0 +1,73 @@
+package com.fusionspy.beacon.web;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: bao
+ * Date: 13-10-17
+ * Time: 下午11:26
+ * To change this template use File | Settings | File Templates.
+ */
+public class JsonGrid {
+
+    private String total;
+
+    private List<JsonRow> rows = new ArrayList<JsonRow>();
+
+    public String getTotal() {
+        return total;
+    }
+
+    public void setTotal(String total) {
+        this.total = total;
+    }
+
+    public List<JsonRow> getRows() {
+        return rows;
+    }
+
+    public void addRow(JsonRow row) {
+        rows.add(row);
+    }
+
+    public static class JsonRow {
+        private String id;
+
+        private List<String> cell = new ArrayList<String>();
+
+        public String getId() {
+            return id;
+        }
+
+        public void setId(String id) {
+            this.id = id;
+        }
+
+        public List<String> getCell() {
+            return cell;
+        }
+
+        public void addCell(String cell) {
+            this.cell.add(cell);
+        }
+    }
+
+    public static interface JsonRowHandler<T> {
+
+        public JsonRow buildRow(T t);
+    }
+
+    public static JsonGrid buildGrid(List recordList, JsonRowHandler handler) {
+        if(recordList!=null) {
+            JsonGrid grid = new JsonGrid();
+            grid.setTotal(recordList.size()+"");
+            for(Object o : recordList) {
+                grid.addRow(handler.buildRow(o));
+            }
+            return grid;
+        }
+        return null;
+    }
+}
